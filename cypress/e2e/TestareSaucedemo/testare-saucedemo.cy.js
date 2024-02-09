c80e92cce4b2e1b62783381e3d2a9eed74388ac7
Daniel Conț <114297966+DanielCont2001@users.noreply.github.com>
2024-02-09 11:58:59 +0200

    testare-saucedemo.cy.js

--- cypress/e2e/TestareSaucedemo/testare-saucedemo.cy.js
+++ cypress/e2e/TestareSaucedemo/testare-saucedemo.cy.js
@@ -61,7 +61,47 @@ describe('On Saucedemo', () => {
         cy.get('.shopping_cart_link').click();
         cy.get('.cart_item').should('exist');
         cy.get('[data-test="remove-sauce-labs-backpack"]').click();
-        cy.get('.cart_item').shouldnot('exist');
+        cy.get('.cart_item').should('not.exist');
+
+    })
+
+    it('Test if you place a order', () => {
+        cy.visit('https://www.saucedemo.com');
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click();
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+        cy.get('.shopping_cart_link').click();
+        cy.get('.cart_item').should('exist');
+        cy.get('[data-test="checkout"]').click();
+        cy.get('[data-test="firstName"]').type('standard');
+        cy.get('[data-test="lastName"]').type('user');
+        cy.get('[data-test="postalCode"]').type('1234');
+        cy.get('[data-test="continue"]').click();
+        cy.get('[data-test="finish"]').click();
+        cy.get('.complete-header').should('exist');
+
+    })
+
+    it('Test to check if you can access the page with the details of a product', () => {
+        cy.visit('https://www.saucedemo.com');
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click();
+        cy.get('#item_4_title_link > .inventory_item_name').click();
+        cy.get('#inventory_item_container').should('exist');
+        cy.get('.inventory_details_desc').should('exist');
+
+    })
+
+    it('Test if the "Back to products" button on a product page takes you to homepage', () => {
+        cy.visit('https://www.saucedemo.com');
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click();
+        cy.get('#item_4_title_link > .inventory_item_name').click();
+        cy.get('[data-test="back-to-products"]').click();
+        cy.get(':nth-child(2) > :nth-child(1) > #inventory_container').should('exist');
 
     })
 
